3ce1982ae3d7c43e370802c006b0f48aed6653a7
Rayve1 <133879405+Rayve1@users.noreply.github.com>
2025-02-18 13:28:04 +0400

    added slider.js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,13 @@
 <x-layout>
-    <div class="flex justify-center w-full h-full">
+    <div class="flex justify-center w-full h-full container">
+        <div id="next_btn" class="w-40 h-40 bg-black"></div>
+        <div id="slider-item-1" class="bg-gray-500 w-full h-1/3 flex justify-between gap-14 visible" ></div>
 
-        <div class="bg-gray-950 w-full h-1/3 ">a</div>
-        <div class=" bg-gray-950 w-full h-1/3 ">a</div>
-        <div class=" bg-gray-950 w-f h-1/3 ">a</div>
+        <div id="slider-item-2" class=" bg-red-500 w-full h-1/3 hidden"></div>
 
-    </div>
-</x-layout>
+        <div id="slider-item-3" class=" bg-blue-500 w-full h-1/3 hidden"></div>
+        <div id="prev_btn" class="w-40 h-40 bg-black"></div>
+    </div>  
+
+    
+</x-layout> 
