710e76af2827cf8bc83bbb8f90647b9d2aff303b
Rayve1 <133879405+Rayve1@users.noreply.github.com>
2025-02-18 13:42:01 +0400

    slider done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 <x-layout>
     <div class="flex justify-center w-full h-full container">
         <div id="next_btn" class="w-40 h-40 bg-black"></div>
-        <div id="slider-item-1" class="bg-gray-500 w-full h-1/3 flex justify-between gap-14 visible" ></div>
+        <div id="slider-item-1" class="bg-gray-500 w-full h-2/3 flex justify-between gap-14 visible" ></div>
 
-        <div id="slider-item-2" class=" bg-red-500 w-full h-1/3 hidden"></div>
+        <div id="slider-item-2" class=" bg-red-500 w-full h-2/3 hidden"></div>
 
-        <div id="slider-item-3" class=" bg-blue-500 w-full h-1/3 hidden"></div>
+        <div id="slider-item-3" class=" bg-blue-500 w-full h-2/3 hidden"></div>
         <div id="prev_btn" class="w-40 h-40 bg-black"></div>
     </div>  
 
