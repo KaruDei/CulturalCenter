f94a0a236313f4e7914f6a27b87a0cb557164fbe
Rayve1 <133879405+Rayve1@users.noreply.github.com>
2025-02-18 12:43:44 +0400

    trash

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 
         <div class="bg-gray-950 w-full h-1/3 ">a</div>
         <div class=" bg-gray-950 w-full h-1/3 ">a</div>
-        <div class=" bg-gray-950 w-f h-1/3 ">a</div>
+
 
     </div>
 </x-layout>
