06f65ed78a5b3a9bc3041d69c2b841d8004e30bf
Rayve1 <133879405+Rayve1@users.noreply.github.com>
2025-02-20 22:09:15 +0400

    added .css file

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,10 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }}</title>
-    @vite(['resources\css\app.css', 'resources\js\app.js'])
+    @vite([
+        'resources\css\app.css',
+        'resources\js\app.js',
+        'resources\css\main-page.css
+        '])
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('resources/css/main-page.css') }}">
 </head>
 <body class="h-full bg-linen text-gray-800">
     <div class="flex flex-col h-full">
