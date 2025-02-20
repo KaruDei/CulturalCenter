a5605ad5a2214a950cb9785b812e83e159311ed3
KaruDei <1myoffice13@gmail.com>
2025-02-20 17:51:35 +0400

    EventController done

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,15 +10,13 @@ class EventController extends Controller
     // Получение всех записей
     public function GetAllEventRecords()
     {
-        $events = Events::all();
-        return view('events', ['events' => $events]);
+        return Events::all();
     }
 
     // Получение одной записи
     public function GetEventRecord($id)
     {
-        $event = Events::get()->where('id', $id);
-        return view('event', ['event' => $event]);
+        return Events::findOrFail($id);
     }
 
     // Создание новой записи
@@ -43,9 +41,9 @@ class EventController extends Controller
         {
             $image = $request->file('poster');
             $name = time() . '-' . $image->getClientOriginalName();
-            $destinationPath = public_path('/images');
+            $destinationPath = public_path('/images/events');
             $image->move($destinationPath, $name);
-            $fields['poster'] = '/images/' . $name;
+            $fields['poster'] = '/images/events/' . $name;
         }
 
         Events::create($fields);
@@ -56,13 +54,55 @@ class EventController extends Controller
     // Изменение записи
     public function UpdateEvent(Request $request, $id)
     {
-        
+        $fields = $request->validate([
+            'title' => 'required|min:3|max:255|unique:events,title,' . $id,
+            'description' => 'required|min:3',
+            'id_event_type' => 'required|numeric',
+            'id_room' => 'required|numeric',
+            'date' => 'required',
+            'time' => 'required',
+            'duration' => 'required|numeric|gte:0',
+            'price' => 'required|numeric|gte:0',
+            'poster' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'id_event_script' => 'required|numeric',
+            'id_creator' => 'required|numeric',
+            'id_event_status' => 'required|numeric'
+        ]);
+
+        $event = Events::findOrFail($id);
+
+        if (!$event)
+        {
+            return redirect()->back()->with('error', 'Мероприятие не найденно!');
+        }
+
+        if ($request->hasFile('poster'))
+        {
+            $image = $request->file('poster');
+            $name = time() . '-' . $image->getClientOriginalName();
+            $destinationPath = public_path('/images/events');
+            $image->move($destinationPath, $name);
+            $fields['poster'] = '/images/events/' . $name;
+        }
+        else 
+        {
+            $fields['poster'] = $event->poster;
+        }
+
+        $event->update($fields);
+
+        return redirect()->back()->with('success', 'Мероприятие изменено!');
     }
 
     // Удаление записи
     public function DeleteEvent($id)
     {
-        Events::where('id', $id)->delete();
-        return redirect()->back()->with('success', 'Мероприятие удалено!');
+        $event = Events::findOrFail($id);
+        try {
+            $event->delete();
+            return redirect()->back()->with('success', 'Мероприятие удалено!');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Ошибка при удалении мероприятия: ' . $e->getMessage());
+        }
     }
 }
