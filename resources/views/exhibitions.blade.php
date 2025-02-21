3ee5ff17cc21adc3f2d109f5aa02221582e14bb2
Rayve1 <133879405+Rayve1@users.noreply.github.com>
2025-02-21 17:34:46 +0400

    come fix

--- resources/views/exhibitions.blade.php
+++ resources/views/exhibitions.blade.php
@@ -1,13 +1,14 @@
 <x-layout>
-
-<div class="flex flex-col container items-stretch ml-auto mr-auto mt-[-80px]">
+<div class="flex flex-col container items-stretch ml-auto mr-auto">
                 <h1 class="text-5xl font-medium text-teal-800 mb-10 mt-10 mx-auto ">Выставки</h1>
                 <div class="grid grid-cols-4 gap-5">
                     @foreach ( $exhibitions as $exhibition)
-                    <a href="{{Route('exhibition', $exhibition['id'])}}" class=" bg-floral-white rounded-xl shadow-2xl max-w-4xl border border-amber-100  w-[400fr] h-[600px] flex flex-col items-center overflow-hidden">                     
-                        <img src="{{$exhibition['picture']}}"  class="w-full h-2/4">
-                        <h2 class="mt-3 mb-3">{{$exhibition['title']}}</h2>
-                        <p class="ml-5 mr-5">{{$exhibition['description']}}</p>                       
+                    <a href="{{Route('exhibition', $exhibition['id'])}}" class="event-item">                     
+                        <img src="{{$exhibition['picture']}}" class="event-img">
+                        <div class="event-text">
+                            <h2>{{$exhibition['title']}}</h2>
+                            <p>{{$exhibition['description']}}</p>      
+                        </div>
                     </a>
                     @endforeach
                 </div>
