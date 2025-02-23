6fc07a6946a52824615898bc114c183d2010f8cb
Rayve1 <133879405+Rayve1@users.noreply.github.com>
2025-02-23 14:45:53 +0400

    added adaptive

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -2,7 +2,7 @@
 <div class="flex flex-col container items-stretch ml-auto mr-auto">
                 <h1 class="text-5xl font-medium text-teal-800 mt-10 mx-auto ">Все мероприятия</h1>
 
-                <div class="grid grid-cols-4 gap-5">
+                <div class=" grid-cols-4 gap-5 block md:grid">
                     
                     @foreach ($events as $event)
                     <a href="{{Route('event', $event['id'])}}" class="event-item">                     
--- resources/views/exhibitions.blade.php
+++ resources/views/exhibitions.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 <div class="flex flex-col container items-stretch ml-auto mr-auto">
                 <h1 class="text-5xl font-medium text-teal-800 mb-10 mt-10 mx-auto ">Выставки</h1>
-                <div class="grid grid-cols-4 gap-5">
+                <div class="grid-cols-4 gap-5 block md:grid">
                     @foreach ( $exhibitions as $exhibition)
                     <a href="{{Route('exhibition', $exhibition['id'])}}" class="event-item">                     
                         <img src="{{$exhibition['picture']}}" class="event-img">
