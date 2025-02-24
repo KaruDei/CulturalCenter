512468bc6f603069e6f912907bd22a53c7a0720f
Rayve1 <133879405+Rayve1@users.noreply.github.com>
2025-02-24 18:06:34 +0400

    Added profile modal window

---
+++ resources/js/profileModal.js
@@ -0,0 +1,23 @@
+try {
+const modal = document.querySelector(".modalWindow-container")
+const closeButton =document.querySelector(".button-close")
+let modalBody = document.querySelector(".modalWindow-body")
+let labelID = document.getElementById("labelID")
+let inputFullname = document.getElementById("inputFullname")
+let inputEmail = document.getElementById("inputEmail")
+let labelPicture = document.getElementById("labelPicture")
+const button = document.getElementById("button-edit-profile");
+console.log(button)
+button.addEventListener('click', function() {
+    let a = button.id
+    let parentDiv = button.parentElement.parentElement.parentElement;
+    modal.style.display = "flex"
+});
+
+
+closeButton.addEventListener('click', function(){
+     modal.style.display = "none"
+})
+} catch (error) {
+    
+}
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,4 +1,32 @@
 <x-layout>
+
+    <div class="modalWindow-container">
+        <div class="modalWindow-background">
+            
+        </div>
+        <form action="{{Route('admin.users')}}" method="POST" class="modalWindow-body", enctype="multipart/form-data">
+            @csrf
+            @method("patch")
+            <p class="button-close">Закрыть</p>
+            <label for="fullname">
+                ФИО
+            </label>
+            <input type="hidden" name="user_id" id="labelID" class="input-modal" value="{{$user->id}}">
+            <input type="text" name="full_name" id="inputFullname" class="input-modal" value="{{$user->full_name}}">
+            <label for="email">
+                почта
+            </label>
+            <input type="text" name="email" id="inputEmail" class="input-modal" value="{{$user->email}}">
+            <label for="inputPassword">Пароль</label>
+            <input type="password" name="password" id="inputPassword" class="input-modal" value="{{$user->password}}">
+            <label for="picture">
+                <img id="labelPicture" src="{{$user->picture}}" alt="asdasd" class="w-12 h-12 rounded-full object-cover">
+            </label>
+            <input type="file" name="picture" id="inputPicture" class="input-modal">
+            <input type="submit" name="" id="" class="input-modal">
+        </form>
+    </div>
+
     <div class="mt-14 container-info">
         <div class="all-info">
             <h1 class="auth-title">Ваш личный кабинет</h1>
@@ -19,7 +47,7 @@
                         <p class="user-detail">
                             <span class="detail-label">Email:</span> {{$user->email}}
                         </p>
-                        <button class="edit-button">Редактировать профиль</button>
+                        <button id="button-edit-profile" class="edit-button">Редактировать профиль</button>
                     </div>
                 </div>
             </section>
