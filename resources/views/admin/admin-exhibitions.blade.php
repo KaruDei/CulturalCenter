9424a56c6d9384b5e9981e1b0adf9e3c27b5ffde
Rayve1 <133879405+Rayve1@users.noreply.github.com>
2025-02-26 18:27:43 +0400

    pochti gotoviy exhibitionModal

--- resources/views/admin/admin-exhibitions.blade.php
+++ resources/views/admin/admin-exhibitions.blade.php
@@ -1,4 +1,41 @@
 <x-admin-layout>
+    <div class="modalWindow-container">
+        <div class="modalWindow-background">
+            
+        </div>
+        <form action="{{Route('admin.users')}}" method="POST" class="modalWindow-body">
+            @csrf
+            @method("patch")
+            <p class="button-close">Закрыть</p>
+            
+            <input type="hidden" id="exhibitionID">
+            
+            <label for="title" class="form-label">Название</label>
+            <input type="text" name="title" id="inputTitle" class="form-input" placeholder="Введите Название выставки" required>
+
+             
+            <label for="description" class="form-label">Описание</label>
+            <input type="text" name="description" id="inputDescription" class="form-input" placeholder="Введите описание выставки" required>
+
+             
+            <label for="picture" class="form-label">Картинка</label>
+            <input type="file" name="picture" id="inputPicture" class="form-input" placeholder="Картинка выставки" required>
+
+             
+            <label for="start_date" class="form-label">Стартовая дата</label>
+            <input type="date" name="start_date" id="inputStartDate" class="form-input" placeholder="Введите стартовую дату выставки" required>
+
+             
+            <label for="end_date" class="form-label">Конечная дата</label>
+            <input type="date" name="end_date" id="inputEndDate" class="form-input" placeholder="Введите стартовую выставки выставки" required>
+
+            
+            <input type="submit" value="Сохранить" class="submit-button">
+            
+        </form>
+        
+        
+    </div>
     <div class="flex flex-col items-center justify-start min-h-screen bg-linen mt-14 mb-12 pt-20">
 
         <h1 class="text-4xl font-bold text-teal-800 mb-12 border-b-4 border-amber-300 pb-4 text-center">Все выставки</h1>
@@ -25,6 +62,7 @@
                         $i++; 
                     @endphp
                     <tr>
+                        
                         <td class="py-2 px-4 border-b">{{$exhibition->title}}</td>
                         <td class="py-2 px-4 border-b text-left">
                             <div class="w-60 h-40 overflow-y-auto">
@@ -38,9 +76,10 @@
                         <td class="py-2 px-4 border-b">{{$exhibition->end_date}}</td>
                         <td class="py-2 px-4 border-b">{{$exhibition->created_at}}</td>
                         <td class="py-2 px-4 border-b">{{$exhibition->updated_at}}</td>
+                        <td class="py-2 px-4 border-b hidden">{{$exhibition->id}}</td>
                         <td class="py-2 px-4 border-b">
                             <div class="flex space-x-2">
-                                <button class="bg-white hover:bg-gray-100 text-teal-700 font-semibold py-2 px-4 border border-teal-500 rounded transition duration-300">Редактировать</button>
+                                <button id="admin-exhibition-button-edit-{{$i}}" class=" bg-white hover:bg-gray-100 text-teal-700 font-semibold py-2 px-4 border border-teal-500 rounded transition duration-300">Редактировать</button>
                                 <button class="bg-white hover:bg-gray-100 text-red-700 font-semibold py-2 px-4 border border-red-500 rounded transition duration-300">Удалить</button>
                             </div>
                         </td>
